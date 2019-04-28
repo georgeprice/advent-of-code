c9e779adfd016cc094f14a611e33995b39dba52c
George Price <georgepriiice@gmail.com>
2019-04-28 19:37:38 +0100

    Day 2, 2017: Fewer lines :)

--- 2017/day-2.py
+++ 2017/day-2.py
@@ -1,7 +1,5 @@
 def solve(x: list) -> int:
-    if len(x) == 0:
-        return 0
-    return max(x[0]) - min(x[0]) + solve(x[1:])
+    return 0 if len(x) == 0 else max(x[0]) - min(x[0]) + solve(x[1:])
 
 if __name__ == "__main__":
     assert solve([[5, 1, 9, 8], [7, 5, 3], [2, 4, 6, 8]]) == 18
