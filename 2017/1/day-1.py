acc3268d28dc3a1e4555ae9f13ab156d93db3ef1
George Price <georgepriiice@gmail.com>
2019-04-28 19:32:21 +0100

    Day 1, 2017: Initial

---
+++ 2017/1/day-1.py
@@ -0,0 +1,14 @@
+def solve(x: str) -> int:
+    return sum(map(lambda x: int(x[0]), filter(_match, zip(_shift(x), x))))
+
+def _match(pair_tuple: tuple) -> bool:
+    return pair_tuple[0] == pair_tuple[1]
+
+def _shift(x: str, c: int = 1) -> int: 
+    return x if c == 0 else _shift(x[-1] + x[:len(x) - 1], c - 1)
+    
+if __name__ == "__main__":
+    assert solve("1122") == 3
+    assert solve("1111") == 4
+    assert solve("1234") == 0
+    assert solve("91212129") == 9
