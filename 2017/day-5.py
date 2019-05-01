80774e7d44d76e3f75e44e5ea965df918116304e
George Price <georgepriiice@gmail.com>
2019-05-01 17:20:26 +0100

    Day 5, 2017: Just a while loop, could do with more testing

---
+++ 2017/day-5.py
@@ -0,0 +1,9 @@
+def solve(instructions: list) -> int:
+    pc = 0
+    while pc < len(instructions):
+        instructions[pc] += 1
+        pc += instructions[pc] - 1
+    return pc
+
+if __name__ == "__main__":
+    assert solve([0, 3, 0, 1, -3]) == 5
