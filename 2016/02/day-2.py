d06a55c7f621fb6b6628a2b335ebe77e11daf932
George Price <georgepriiice@gmail.com>
2021-04-19 11:12:10 +0100

    day 2, 2016 - added 2nd part, refactored 1st part

--- 2016/02/day-2.py
+++ 2016/02/day-2.py
@@ -1,44 +1,120 @@
+import enum
 from functools import reduce
-
-KEY_PAD = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-
-def solve(commands: list) -> list:
-
-    # starting on the '5' key
-    state = (1, 1)
-
-    for command in commands:
-        state = reduce(_update_state, command, state)
-        yield _to_keys(state)
+import typing
 
 
-def _to_keys(coordinates: (int, int)) -> int:
-    """
-    Converts (x, y) coordinates to the equivalent integer keys on a keypad
-    :param coordinates: an (x, y) coordinate tuple
-    :return: the integer key found on the keypad at that position
-    """
-    x, y = coordinates
+class Command(enum.Enum):
+    Up = "U"
+    Down = "D"
+    Left = "L"
+    Right = "R"
+
+
+CommandHandler = typing.Callable[[str, Command], str]
+
+
+def solve(actions: list, initial: str, handler: CommandHandler) -> list:
+    states = []
+    for action in actions:
+        for command in map(Command, action):
+            initial = handler(initial, command)
+        states.append(initial)
+    return states
+
+
+KEY_PAD = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+_keypad_lut = {
+    "1": (0, 0),
+    "2": (1, 0),
+    "3": (2, 0),
+    "4": (0, 1),
+    "5": (1, 1),
+    "6": (2, 1),
+    "7": (0, 2),
+    "8": (1, 2),
+    "9": (2, 2),
+}
+
+
+def _keypad_handler(state: str, command: Command) -> str:
+    x, y = _keypad_lut[state]
+    if command is command.Up:
+        x, y = x, max(0, y - 1)
+    elif command is command.Down:
+        x, y = x, min(2, y + 1)
+    elif command is Command.Left:
+        x, y = max(0, x - 1), y
+    elif command is Command.Right:
+        x, y = min(2, x + 1), y
     return KEY_PAD[y][x]
 
 
-def _update_state(start: (int, int), command: str) -> (int, int):
-    """
-    Returns a new (x, y) state given a start state and command (U/D/L/R)
-    :param start: the original x, y state
-    :param command: a movement command to be performed
-    :return: a new (x, y) state after performing
-    """
-    x, y = start
-    if command == 'U':
-        return x, max(0, y - 1)
-    if command == 'D':
-        return x, min(2, y + 1)
-    if command == 'L':
-        return max(0, x - 1), y
-    if command == 'R':
-        return min(2, x + 1), y
+_advanced_keypad_lut = {
+    "1": {
+        Command.Down: "3",
+    },
+    "2": {
+        Command.Down: "6",
+        Command.Right: "3",
+    },
+    "3": {
+        Command.Up: "1",
+        Command.Down: "7",
+        Command.Right: "4",
+        Command.Left: "2",
+    },
+    "4": {
+        Command.Down: "8",
+        Command.Left: "3",
+    },
+    "5": {
+        Command.Right: "6",
+    },
+    "6": {
+        Command.Up: "2",
+        Command.Down: "A",
+        Command.Right: "7",
+        Command.Left: "5",
+    },
+    "7": {
+        Command.Up: "3",
+        Command.Down: "B",
+        Command.Right: "8",
+        Command.Left: "6",
+    },
+    "8": {
+        Command.Up: "4",
+        Command.Down: "C",
+        Command.Right: "9",
+        Command.Left: "7",
+    },
+    "9": {
+        Command.Left: "8",
+    },
+    "A": {
+        Command.Up: "6",
+        Command.Right: "B",
+    },
+    "B": {
+        Command.Up: "7",
+        Command.Down: "D",
+        Command.Right: "C",
+        Command.Left: "A",
+    },
+    "C": {
+        Command.Up: "8",
+        Command.Left: "B",
+    },
+    "D": {
+        Command.Up: "B",
+    },
+}
+
+
+def _advanced_keypad_handler(state: str, command: Command) -> str:
+    if command in _advanced_keypad_lut[state]:
+        return _advanced_keypad_lut[state][command]
+    return state
 
 
 if __name__ == "__main__":
